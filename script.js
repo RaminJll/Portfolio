fd06ae7cab28e4470a5aa6b84822c67359e8206f
Ramin Jalili <raminjalili2507@gmail.com>
2026-02-10 08:20:14 +0100

    Terminé

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+// Fermer le menu quand on clique sur un lien
+document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
+
 const currentYear = new Date().getFullYear();
 const footerYear = document.querySelector('.droit');
 footerYear.innerHTML = `&copy; ${currentYear} Ramin Jalili. Tous droits réservés.`;
